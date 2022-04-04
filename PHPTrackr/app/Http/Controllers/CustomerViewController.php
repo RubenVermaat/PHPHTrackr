f14412fdfca5ed55b2e2ca5278edd62d089674b0
RubenvanderHout <ruben.vanderhout@gmail.com>
2022-04-04 18:54:30 +0200

    sorting

--- PHPTrackr/app/Http/Controllers/CustomerViewController.php
+++ PHPTrackr/app/Http/Controllers/CustomerViewController.php
@@ -4,18 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Label;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CustomerViewController extends Controller
 {
-    public function show()
+    public function index()
     {
-        $data = Label::paginate(2);
+        $data = 0;
+    
+        if(request('name')){
+            $data = DB::table('labels')->join('packages', 'labels.packageId', '=', 'packages.id')
+            ->orderBy(request('name'))
+            ->select(['labels.id','packages.firstname', 'packages.email','labels.shop', 'labels.status'])
+            ->paginate(2);
+        } else if(request('search')){
+           
+            $data = DB::table('labels')->join('packages', 'labels.packageId', '=', 'packages.id')
+            ->where('packages.firstname', 'like', '%' . request('search') . '%')
+            ->orWhere('packages.email', 'like', '%' . request('search') . '%')
+            ->orWhere('labels.shop', 'like', '%' . request('search') . '%')
+            ->orWhere('labels.status', 'like', '%' . request('search') . '%')
+            ->select(['labels.id','packages.firstname', 'packages.email','labels.shop', 'labels.status'])
+            ->paginate(2);
+        } else{
+            $data = DB::table('labels')->join('packages', 'labels.packageId', '=', 'packages.id')
+            ->select(['labels.id','packages.firstname', 'packages.email','labels.shop', 'labels.status'])
+            ->paginate(2);
+        }
+        
         return view('customers.index',  ['data' => $data]);
     }
 
     
-    public function store(Request $request)
-    {
-       
-    }
 }
