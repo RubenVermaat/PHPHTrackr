62ba7f379eaa6f0f5f9d07059ce12473b34bc45e
RubenVermaat <rubenvermaat01@gmail.com>
2022-04-05 13:40:29 +0200

    Improved style

--- PHPTrackr/resources/views/packages/index.blade.php
+++ PHPTrackr/resources/views/packages/index.blade.php
@@ -9,11 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="{{ route('packageSearch') }}" > 
-                        @csrf
-                        <input name="search"> 
-                        <input type="submit" value="Search">
-                    </form>
+                    <div class="mt-8 mb-8 h-8">
+                        <form method="POST" action="{{ route('packageSearch') }}" > 
+                            @csrf
+                            <x-input name="search" placeholder="Search" class="border-gray-200 border h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"></x-input>
+                            <input type="submit" value="Search">
+                        </form>
+                    </div>
                     <a href="/packages/create">Create package </a>
                     <x-adminPanel.list-packages :data="$packages" :sortable="true">
 
